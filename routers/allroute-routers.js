0bbe198484a565258690c5b80da9e75723950f82
cgl091995 <cgl1995.9@gmail.com>
2024-10-21 22:47:26 +0200

    validating

--- routers/allroute-routers.js
+++ routers/allroute-routers.js
@@ -2,6 +2,9 @@
 const express=require("express");
 //guardo la clase router del objeto express en variable router
 const router=express.Router();
+const{check}=require('express-validator')
+const {validateInputs} = require('../middleware/validate.js')
+
 
 //importo el objeto desestructurado de funciones de controladores
 const {getIndice,
@@ -15,7 +18,8 @@ const {getIndice,
     createServicios,    
     updateServicios,
     deleteServicios
-} = require ("../controllers/front-controllers.js")
+} = require ("../controllers/front-controllers.js");
+
 
 router.get("/", getIndice)
 
@@ -29,9 +33,15 @@ router.get("/productos", getProductos)
 //GET method by ID
 router.get("/productos/:id", getProductosByID)
 //POST method: crear datos / status 201
-router.post("/productos", createProductos)
+router.post("/productos", [
+    check( 'producto', 'El servicio es obligatorio').not().isEmpty(),
+    check( 'descripcion', 'La descripcion es obligatoria') .not().isEmpty(),
+    validateInputs],createProductos)
 //PUT method by ID: uddate datos / status 200
-router.put("/productos/:id", updateProductos)
+router.put("/productos/:id", [
+    check( 'producto', 'El servicio es obligatorio').not().isEmpty(),
+    check( 'descripcion', 'La descripcion es obligatoria') .not().isEmpty(),
+    validateInputs],updateProductos)
 //DELETE method by ID: delete datos / status 200
 router.delete("/productos/:id", deleteProductos)
 
@@ -45,9 +55,15 @@ router.get("/servicios", getServicios)
 //GET method by ID
 router.get("/servicios/:id", getServiciosByID)
 //POST method: crear datos / status 201
-router.post("/servicios", createServicios)
+router.post("/servicios",[
+check( 'servicio', 'El servicio es obligatorio').not().isEmpty(),
+check( 'descripcion', 'La descripcion es obligatoria') .not().isEmpty(),
+validateInputs], createServicios)
 //PUT method by ID: uddate datos / status 200
-router.put("/servicios/:id", updateServicios)
+router.put("/servicios/:id", [
+    check( 'servicio', 'El servicio es obligatorio').not().isEmpty(),
+    check( 'descripcion', 'La descripcion es obligatoria') .not().isEmpty(),
+    validateInputs],updateServicios)
 //DELETE method by ID: delete datos / status 200
 router.delete("/servicios/id:", deleteServicios)
 
